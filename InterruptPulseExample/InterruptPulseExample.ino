1a422b84db345071b5f6f91496fa7a10fd498568
Indrek Luuk <indrek.luuk@gmail.com>
2021-03-02 21:05:49 +0200

    Better comments

--- InterruptPulseExample/InterruptPulseExample.ino
+++ InterruptPulseExample/InterruptPulseExample.ino
@@ -1,5 +1,6 @@
-
-volatile int period = 25; // X * 64 * 0.0625us. (16Mhz = 0.0625us, prescaler = 64)
+// 1/16Mhz = 0.0625us, prescaler = 64
+// Pulse time = period * 64 * 0.0625us
+volatile int period = 25; // Pulse time = 25 * 64 * 0.0625 = 100us
 
 void setup() {
   noInterrupts();
@@ -7,11 +8,10 @@ void setup() {
   TCCR1B = 0;
   TCNT1  = 0; //initialize counter value to 0
 
-  // X * 64 * 0.0625us. (16Mhz = 0.0625us, prescaler = 64)
   TCCR1B |= (1 << WGM12);   // CTC mode
   TCCR1B |= (1 << CS11) | (1 << CS10);    // 64 prescaler
   TIMSK1 |= (1 << OCIE1A);  // enable timer compare interrupt
-  OCR1A = period - 1; // 200us
+  OCR1A = period - 1;
   interrupts();
 
   pinMode(8, OUTPUT);
@@ -29,8 +29,12 @@ void loop() {
   int potVal = analogRead(A0);
   if ((abs(potVal - lastPotVal) > 5)) {
     lastPotVal = potVal;
-    
+
+    // Pulse time = period * 64 * 0.0625us. (1/16Mhz = 0.0625us, prescaler = 64)
+    // Minimum time = 25 * 64 * 0.0625us = 100us
+    // Maximum time = 125 * 64 * 0.0625us = 500us
     int newPeriod = map(potVal, 0, 1023, 25, 125);
+    
     noInterrupts();
     period = newPeriod;
     interrupts();
